70505c245ece654f5782c8f06a7c05dccd62393b
Frederic Guiet <fguiet@gmail.com>
2018-04-15 11:23:10 +0200

    add docker stuff

---
+++ third-party-tools/influxdb/docker/docker-entry-point.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -e
+
+RESTORE_DIR=/influxdb/restore
+DATA_DIR=/influxdb/data
+META_DIR=/influxdb/meta
+INFLUXDB_DAEMON_DIR=/usr/bin/influxd
+CONF_FILE=/influxdb/conf/influxdb.conf
+LOG_FILE=/influxdb/logs/influxdb.log
+
+echo "Checking for backup to restore"
+if [ -z "$(ls -A ${RESTORE_DIR})" ]; then
+  echo "Restore dir empty...nuffin to do  !"
+else
+  echo "Restore dir not empty...will try to restore database..."
+  echo "Restoring metastore..."
+  ${INFLUXDB_DAEMON_DIR} restore -metadir ${META_DIR} ${RESTORE_DIR}
+  echo "Restoring database automation..."
+  ${INFLUXDB_DAEMON_DIR} restore -database automation -datadir ${DATA_DIR} ${RESTORE_DIR}
+  echo "Removing restore files"
+  rm /influxdb/restore/*
+fi
+
+echo "Now starting Influxdb..."
+ ${INFLUXDB_DAEMON_DIR} -config ${CONF_FILE} 2>&1 ${LOG_FILE}
