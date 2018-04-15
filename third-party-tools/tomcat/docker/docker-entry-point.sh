cc69cf6a9c60a0ab758a1afa450a32bb8345244a
fguiet <fguiet@gmail.com>
2018-04-15 20:10:57 +0200

    add tomcat dockerfile

---
+++ third-party-tools/tomcat/docker/docker-entry-point.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+TOMCAT_DEPLOY_FOLDER=/tomcat/deploy
+TOMCAT_WEBAPP_DIR=/tomcat/apache-tomcat-9.0.7/webapps
+
+echo "Checking whether webapp deployment is necessary"
+if [ -z "$(ls -A ${TOMCAT_DEPLOY_FOLDER})" ]; then
+  echo "Deploy folder empty...skipping..."  
+else
+  echo "Deploying new webapp..."
+  echo "Removing old webapp..."
+  rm -Rf ${TOMCAT_WEBAPP_DIR}/
+  echo "Copying new wepapp..."
+  cp ${TOMCAT_DEPLOY_FOLDER}/automationserver-webapp.war ${TOMCAT_WEBAPP_DIR}/
+fi
+
+echo "Starting Tomcat server..."
+#$CATALINA_HOME/bin/catalina.sh run
+exec $@
+
