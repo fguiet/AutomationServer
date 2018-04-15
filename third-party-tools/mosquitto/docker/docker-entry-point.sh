c836f0f88bf46b245ddb68a3e6dced40446e3456
Frederic Guiet <fguiet@gmail.com>
2018-04-15 12:20:18 +0200

    add mosquitto docker conf

--- third-party-tools/mosquitto/docker/docker-entry-point.sh
+++ third-party-tools/mosquitto/docker/docker-entry-point.sh
@@ -2,7 +2,7 @@
 set -e
 
 MOSQUITTO_DAEMON_DIR=/usr/sbin/mosquitto
-CONF_FILE=/mosquitto/config/mosquitto.conf
+CONF_FILE=/mosquitto/conf/mosquitto.conf
 
 echo "Now starting Mosquitto..."
 ${MOSQUITTO_DAEMON_DIR} -c ${CONF_FILE}
