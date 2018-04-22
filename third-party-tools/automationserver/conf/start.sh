0bd3be1b6950fb65ef7ef705b0898cdd62b67eee
Frederic Guiet <fguiet@gmail.com>
2018-04-22 19:10:35 +0200

    Fix some scripts

--- third-party-tools/automationserver/conf/start.sh
+++ third-party-tools/automationserver/conf/start.sh
@@ -5,14 +5,16 @@ export EXEC="/usr/bin/jsvc"
 export HOME_PATH="/applications/automationserver"
 export LOG4J="log4j.configuration=file:${HOME_PATH}/config/log4j.xml"
 #export JAVA_HOME="/usr/java/jdk1.8.0_111"
-export JAVA_LIBRARY_PATH="java.library.path=/applications/automationserver"
-export CLASS_PATH="${HOME_PATH}/*"
+export JAVA_LIBRARY_PATH="java.library.path=/applications/automationserver/bin"
+export CLASS_PATH="${HOME_PATH}/bin/*"
 export ARGS="-Dautomationserver.config.path=/applications/automationserver/config/automationserver.properties"
 export LOG_OUT="${HOME_PATH}/logs/${NAME}.out"
 export LOG_ERR="${HOME_PATH}/logs/${NAME}.err"
 export PID="${HOME_PATH}/${NAME}.pid"
 export CLASS=fr.guiet.automationserver.AutomationServer
 
-rm /var/lock/LCK..ttyUSB0
-$EXEC -home "$JAVA_HOME" -cp "$CLASS_PATH" -D"${JAVA_LIBRARY_PATH}" -D"${LOG4J}" $ARGS -outfile "${LOG_OUT}" -errfile "${LOG_ERR}" -pidfile "${PID}" $1 $CLASS
+#rm /var/lock/LCK..ttyUSB0
+#remove pid because sometime when Raspberry reboot it is not removed and prevent automationserver from starting
+rm ${PID}
+$EXEC -nodetach -server -jvm server -java-home "$JAVA_HOME" -cwd "/applications/automationserver/bin"  -cp "$CLASS_PATH" -D"user.dir=/tmp" -D"${JAVA_LIBRARY_PATH}" -D"${LOG4J}" $ARGS -outfile "${LOG_OUT}" -errfile "${LOG_ERR}" -pidfile "${PID}" $1 $CLASS
 
--- third-party-tools/postgresql/install/readme.txt
+++ third-party-tools/postgresql/install/readme.txt
@@ -3,8 +3,8 @@ Starting Postgresql docker
 
 1. Run AutomationServer/third-party-tools/postgresql/install/prerequisite
 2. Make old postgresl backups
-	pg_dump --globals-only > users_20180409.sql
-	pg_dump automation > automation_dump_20180409.sql
+	pg_dumpall --globals-only > users_20180409.sql
+	pg_dump --create automation > automation_dump_20180409.sql
 	
 3. Put backups in applications/postgresql/restore/
 4. Launch postgresql docker : AutomationServer/third-party-tools/postgresql/docker/postgresql_dockerrun
