27c1b36622680ad6d4fc53f194700cd92ae74615
Guiet Frederic <F.Guiet@brgm.fr>
2018-12-27 15:43:01 +0100

    add ble hub

---
+++ sketches/homehub/homehub.ino
@@ -0,0 +1,299 @@
+/*
+ * This program use a modified version of :
+ *    https://github.com/nkolban/ESP32_BLE_Arduino
+ *    
+ * In fact, this library does not have getServiceNb() function and getServiceData(int i)  
+ */
+
+#include "BLEDevice.h"
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define SENSORS_COUNT 2
+#define DEBUG 1
+
+const char* ssid = "DUMBLEDORE";
+const char* password = "frederic";
+
+#define MAX_RETRY 50
+#define MQTT_CLIENT_ID "HubUpstairsMqttClient"
+#define MQTT_SERVER "192.168.1.25"
+#define MQTT_TOPIC "/guiet/inside/sensor"
+
+#define BLE_CLIENT_ID "HubUpstairsBleClient"
+BLEScan* pBLEScan;
+BLEClient* pClient;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+static BLEAddress *pServerAddress;
+
+bool deviceFound = false;
+char message_buff[100];
+
+struct Sensor {
+    String Address;
+    int SensorId;
+    String Temperature;
+    String Humidity;
+    String Battery;
+    bool hasDataToPush;
+};
+
+Sensor sensors[SENSORS_COUNT];
+
+void InitSensors() {
+
+  //String SENSORID =  "1"; //Bureau
+  //String SENSORID =  "2"; //Salon
+  //String SENSORID =  "3"; //Nohe
+  //String SENSORID =  "4"; //Manon
+  //String SENSORID =  "5"; //Parents
+  
+  sensors[0].Address = "d2:48:c8:a5:35:4c";
+  sensors[0].SensorId = 1;
+  sensors[0].hasDataToPush = false;
+
+  sensors[1].Address = "c7:b9:43:94:24:3a";
+  sensors[1].SensorId = 2;
+  sensors[1].hasDataToPush = false;
+  
+}
+
+long StrToHex(char str[])
+{
+  return strtol(str, 0, 16);
+}
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  /**
+      Called for each advertising BLE server.
+  */
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+      
+      pServerAddress = new BLEAddress(advertisedDevice.getAddress());
+
+      for (int i = 0; i < SENSORS_COUNT; i++) {
+        if (strcmp(pServerAddress->toString().c_str(), sensors[i].Address.c_str()) == 0) {
+
+          sensors[i].hasDataToPush = true;
+
+          if (DEBUG) {
+            Serial.printf("Device found with Mac Address : %s \r\n", pServerAddress->toString().c_str());
+            Serial.println("Getting service data...");
+          }
+         
+           if (advertisedDevice.haveServiceData()) {
+              int cpt = advertisedDevice.getServiceDataCount();
+
+              if (DEBUG) {
+                Serial.println("Service data found : " + String(cpt));
+              }
+              
+              char serviceData[10];
+              for(int j=0;j<cpt;j++) {
+                Serial.println("Service n°" + String(j));
+                std::string strServiceData = advertisedDevice.getServiceData(j);                
+                strServiceData.copy(serviceData, strServiceData.length(), 0);
+
+                /* Allocate twice the number of the bytes in the buf array because each byte would be 
+                 * converted to two hex characters, also add an extra space for the terminating null byte
+                 * [size] is the size of the buf array */
+                char output[(strServiceData.length() * 2) + 1];
+                /* pointer to the first item (0 index) of the output array */
+                char *ptr = &output[0];
+                
+                for (int i = 0; i < strServiceData.length(); i++) {
+                    /* sprintf converts each byte to 2 chars hex string and a null byte, for example
+                     * 10 => "0A\0".
+                     *
+                     * These three chars would be added to the output array starting from
+                     * the ptr location, for example if ptr is pointing at 0 index then the hex chars
+                     * "0A\0" would be written as output[0] = '0', output[1] = 'A' and output[2] = '\0'.
+                     *
+                     * sprintf returns the number of chars written execluding the null byte, in our case
+                     * this would be 2. Then we move the ptr location two steps ahead so that the next
+                     * hex char would be written just after this one and overriding this one's null byte.
+                     *
+                     * We don't need to add a terminating null byte because it's already added from 
+                     * the last hex string. */  
+                    ptr += sprintf (ptr, "%02X", serviceData[strServiceData.length()-i-1]);
+                }
+
+                
+                String t = "0x" + String(output);
+                if (DEBUG) {
+                  Serial.println("Hex String : " + t);
+                }               
+                long serviceValue = StrToHex(output);
+                float serviceValueFloat = String(serviceValue).toFloat();
+
+                if (DEBUG) {
+                  printf ("Output (long) : %ld\n", serviceValue);
+                  Serial.println("Output (float) : " + String(serviceValueFloat,2));
+                }
+                  
+                //Temperature
+                if (j == 0)
+                  sensors[i].Temperature = String(serviceValueFloat/100,2);
+
+                //Battery
+                if (j == 1)
+                  sensors[i].Battery = String(serviceValueFloat,0);
+
+                //Humidity
+                if (j == 2)
+                  sensors[i].Humidity = String(serviceValueFloat,0);
+             }
+           }
+        }
+      }
+  
+
+     /* bool known = false;
+      
+      
+        if (known) {
+        Serial.print("Device found: ");
+        Serial.println(advertisedDevice.getRSSI());
+        if (advertisedDevice.getRSSI() > -80) deviceFound = true;
+        else deviceFound = false;
+        Serial.println(pServerAddress->toString().c_str());
+        advertisedDevice.getScan()->stop();
+      }*/
+    }
+}; // MyAdvertisedDeviceCallbacks
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting Arduino BLE Client application...");
+
+  BLEDevice::init(BLE_CLIENT_ID);
+
+  pClient  = BLEDevice::createClient();
+  //Serial.println(" - Created client");
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+}
+
+void loop() {
+
+  InitSensors();
+
+ // Serial.println("Sensors : " + sizeof(sensors));
+  
+  Serial.println();
+  Serial.println("BLE Scan restarted.....");
+  deviceFound = false;
+  BLEScanResults scanResults = pBLEScan->start(20);
+  if (deviceFound) {
+    Serial.println("Found device");
+  }
+
+  pBLEScan->stop();
+  Serial.println("BLE Scan ended.....");
+
+  connectToWifi();
+
+  connectToMqtt();
+
+  //Publish result
+  for (int i=0; i < SENSORS_COUNT;i++) {
+    if (sensors[i].hasDataToPush) {
+      sensors[i].hasDataToPush = false;
+      String mess = "SETINSIDEINFO;"+String(sensors[i].SensorId)+";"+sensors[i].Temperature+";"+sensors[i].Humidity;
+
+      if (DEBUG) {
+        Serial.println("Publishing : " + mess);
+      }
+          
+      mess.toCharArray(message_buff, mess.length()+1);
+      client.publish(MQTT_TOPIC,message_buff);
+    }
+  }
+
+  //Disconnect pro
+  if (client.connected()) {
+    disconnectMqtt();  
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    disconnectWifi();  
+  }
+}
+
+void disconnectMqtt() {
+  Serial.println("Disconnecting from mqtt...");
+  client.disconnect();
+}
+
+void disconnectWifi() {
+  Serial.println("Disconnecting from wifi...");
+  WiFi.disconnect();
+}
+
+void connectToMqtt() {
+  
+  client.setServer(MQTT_SERVER, 1883); 
+
+  int retry = 0;
+  Serial.print("Attempting MQTT connection...");
+  while (!client.connected()) {   
+    if (client.connect(MQTT_CLIENT_ID)) {
+      Serial.println("connected to MQTT Broker...");
+    }
+    else {
+      delay(500);
+      Serial.print(".");
+    }
+
+    
+  }
+
+  if (retry >= MAX_RETRY) {
+    ESP.restart();
+  }
+}
+
+void connectToWifi() 
+{
+  byte mac[6];
+
+  WiFi.disconnect();
+  Serial.println("Connecting to WiFi...");
+//  WiFi.config(ip_wemos, gateway_ip, subnet_mask);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  int retry = 0;
+  while (WiFi.status() != WL_CONNECTED && retry < MAX_RETRY) {
+    delay(500);
+    Serial.print(".");
+    retry++;    
+  }
+
+  if (retry >= MAX_RETRY)
+    ESP.restart();
+  
+  Serial.println ( "" );
+  Serial.print ( "Connected to " );
+  Serial.println ( ssid );
+  Serial.print ( "IP address: " );
+  Serial.println ( WiFi.localIP() );
+  
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+}
