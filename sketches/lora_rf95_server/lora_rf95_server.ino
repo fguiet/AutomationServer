d983d2376c7c619326c41458b36ea770615aa545
Frederic Guiet <F.Guiet@brgm.fr>
2019-05-05 12:53:15 +0200

    add some lora sketches

---
+++ sketches/lora_rf95_server/lora_rf95_server.ino
@@ -0,0 +1,82 @@
+// rf95_reliable_datagram_server.pde
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple addressed, reliable messaging server
+// with the RHReliableDatagram class, using the RH_RF95 driver to control a RF95 radio.
+// It is designed to work with the other example rf95_reliable_datagram_client
+// Tested with Anarduino MiniWirelessLoRa, Rocket Scream Mini Ultra Pro with the RFM95W 
+#include <RHReliableDatagram.h>
+#include <RH_RF95.h>
+#include <SPI.h>
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
+
+#define RFM95_CS D2
+#define RFM95_RST D3
+#define RFM95_INT D1
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 868.0
+
+// Singleton instance of the radio driver
+RH_RF95 driver(RFM95_CS, RFM95_INT);
+
+// Singleton instance of the radio driver
+//RH_RF95 driver;
+//RH_RF95 driver(5, 2); // Rocket Scream Mini Ultra Pro with the RFM95W
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
+// Need this on Arduino Zero with SerialUSB port (eg RocketScream Mini Ultra Pro)
+//#define Serial SerialUSB
+void setup() 
+{
+  // Rocket Scream Mini Ultra Pro with the RFM95W only:
+  // Ensure serial flash is not interfering with radio communication on SPI bus
+//  pinMode(4, OUTPUT);
+//  digitalWrite(4, HIGH);
+  Serial.begin(9600);
+  while (!Serial) ; // Wait for serial port to be available
+  if (!manager.init())
+    Serial.println("init failed");
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+//  driver.setTxPower(23, false);
+  // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
+  // transmitter RFO pins and not the PA_BOOST pins
+  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
+  // Failure to do that will result in extremely low transmit powers.
+//  driver.setTxPower(14, true);
+  // You can optionally require this module to wait until Channel Activity
+  // Detection shows no activity on the channel before transmitting by setting
+  // the CAD timeout to non-zero:
+//  driver.setCADTimeout(10000);
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!driver.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+}
+uint8_t data[] = "And hello back to you";
+// Dont put this on the stack:
+uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+void loop()
+{
+  if (manager.available())
+  {
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
+    {
+      Serial.print("got request from : 0x");
+      Serial.print(from, HEX);
+      Serial.print(": ");
+      Serial.println((char*)buf);
+      // Send a reply back to the originator client
+      if (!manager.sendtoWait(data, sizeof(data), from))
+        Serial.println("sendtoWait failed");
+    }
+  }
+}
