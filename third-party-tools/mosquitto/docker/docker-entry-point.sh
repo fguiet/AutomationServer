961d7159cb9c786404ca32b6a264fbd6fa1014a4
Hypriot God User <fguiet@gmail.com>
2020-02-09 13:14:30 +0100

    Move Mosquitto to official docker hub image v1.6.8, move to docker compose

--- third-party-tools/mosquitto/docker/docker-entry-point.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-MOSQUITTO_DAEMON_DIR=/usr/sbin/mosquitto
-CONF_FILE=/mosquitto/conf/mosquitto.conf
-
-echo "Now starting Mosquitto..."
-${MOSQUITTO_DAEMON_DIR} -c ${CONF_FILE}
