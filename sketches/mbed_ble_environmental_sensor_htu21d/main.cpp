d195171bb04db724440303c9f4467d0a195e3c59
fguiet@gmail.com <fguiet@gmail.com>
2020-10-18 18:05:45 +0200

    update some sketch

---
+++ sketches/mbed_ble_environmental_sensor_htu21d/main.cpp
@@ -0,0 +1,369 @@
+/*
+*
+* Description  : This program advertise some data through GAP Advertising Data
+*               It does not use Standard BLE Services (EnvironmentalService, BatteryService).
+*               So BLE Client are not forced to connect to this BLE Beacon in order to discover services and characteritics associated.                
+*
+*               Careful, before compiling this program, make sure you have altered function addData of GapAvertisingData.h file as follow :
+*                
+*                ble_error_t addData(DataType_t advDataType, const uint8_t *payload, uint8_t len)
+*                {
+*                    //Find field 
+*                    //uint8_t* field = findField(advDataType);
+*            
+*                    //if (field) {
+*                        // Field type already exist, either add to field or replace 
+*                    //    return addField(advDataType, payload, len, field);
+*                    //} else {
+*                        // Field doesn't exists, insert new 
+*                        return appendField(advDataType, payload, len);
+*                    //}
+*                }
+*                                
+*               For more info : 
+*                    - https://os.mbed.com/questions/83683/How-can-add-multiple-SERVICE_DATA/
+*                    - https://os.mbed.com/questions/83675/BLE-advertising-data/
+*
+* Library Requirements : BLE_API, HTU21D, nRF51822, mbed
+* Author               : F.Guiet
+* Creation             : 20181215
+*
+* Modification         : 20190120 - Battery info is now voltage and not percentage, disable ADC to save power
+*                        20190831 - Change Device Name (can only 4 bytes...dunno why) and advertise every 10s (this is the max value) instead of 5s (ADVERTISE_INTERVAL_IN_MS)
+                         20191129 - Improve battery life by disabling advertising for 60s and advertise only for 3s
+*
+*/
+
+//Includes
+#include "mbed.h"
+#include "ble/BLE.h"
+#include "HTU21D.h"
+
+#define THERMOMETER_UUID_SERVICE 0x1809
+#define BATTERY_UUID_SERVICE 0x180F
+//0x181C = User Data = Humidity Storage in our case
+#define USER_DATA_UUID_SERVICE 0x181C
+
+#define DEBUG_MODE 0 //enables serial output for debug, consumes ~1mA when idle
+
+#if DEBUG_MODE
+   // if you see ~1mA consumption during sleep, that's because DEBUG_MODE==1, it's enabled.
+   Serial _device(p9, p11);  //Check to see which pin should be used
+#endif
+
+//Sensor instance
+//DA = P0
+//CL = P1
+HTU21D _sensor(p0,p1);
+
+//mac address for this module
+uint8_t mac_reverse[6] = {0x0,0x0,0x0,0x0,0x0,0x0};  
+
+//device name  
+//Salon F4A4
+//Manon D248
+//Salle de bain D815                                           
+//Parent E93D
+//Bureau D7E0
+//Nohé C7B9
+const static char     DEVICE_NAME[]        = "D7E0";
+
+//const static float    VBAT_IN_V = 3.1;
+
+//to trigger sensor polling in main thread
+static volatile bool triggerSensorPolling = false;
+//to trigger manage avertising in main thread
+static volatile bool triggerManageAvertising = false;
+
+//poll sensor data on a periodic basis
+static Ticker _pollDataTimer; 
+//used to stop advertising after X seconds 
+static Ticker _manageAdvertising;   
+
+//poll data interval
+const uint16_t POLL_DATA_INTERVAL_IN_S = 60; //In seconds
+
+//stop advertising interval
+const uint16_t STOP_ADV_INTERVAL_IN_S = 3; //In seconds
+
+//advertise interval in ms
+//less advertising = less baterry consumption
+//const uint16_t ADVERTISE_INTERVAL_IN_MS = 5000; //In seconds
+//Max value = 10240
+const uint16_t ADVERTISE_INTERVAL_IN_MS = 900; 
+float _temperature = 0;
+float _battery = 0;
+int _humidity = 0;
+
+void generateAdvertisingData(BLE &ble) {
+    
+    /* Setup advertising. */
+    
+    ble.gap().clearAdvertisingPayload();
+    
+    /* set modes "no EDR", "discoverable" for beacon type advertisements */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+    
+    /* List of services */
+   ///ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_16BIT_SERVICE_IDS, (uint8_t *)uuid16_list, sizeof(uuid16_list));
+    
+    //Temperature
+    uint8_t service_data[4];
+    
+    //UUID
+    service_data[0] = THERMOMETER_UUID_SERVICE & 0xff;
+    service_data[1] = THERMOMETER_UUID_SERVICE >> 8;
+    
+    //VALUE
+    
+    //Example
+    //Temp = 20.39 °C
+    //2039 = F7 07 
+    //247 = F7
+    //7 = 07
+    int temp = _temperature * 100;
+    char hex[4];
+    sprintf(hex, "%x", temp);
+    service_data[2] = temp & 0xff;
+    service_data[3] = temp >> 8; 
+    
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::SERVICE_DATA,  (uint8_t *)service_data, sizeof(service_data));
+    
+    //Battery
+    uint8_t service_data2[4];
+    service_data2[0] = BATTERY_UUID_SERVICE & 0xff;
+    service_data2[1] = BATTERY_UUID_SERVICE >> 8;
+    
+    temp = _battery * 100;    
+    sprintf(hex, "%x", temp);
+    service_data2[2] = temp & 0xff;
+    service_data2[3] = temp >> 8;
+            
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::SERVICE_DATA,  (uint8_t *)service_data2, sizeof(service_data2));
+    
+    //Humidity
+    uint8_t service_data3[3];
+    service_data3[0] = USER_DATA_UUID_SERVICE & 0xff;
+    service_data3[1] = USER_DATA_UUID_SERVICE >> 8;
+    service_data3[2] = _humidity;    
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::SERVICE_DATA,  (uint8_t *)service_data3, sizeof(service_data3));
+    
+    /* Appearance */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::GENERIC_THERMOMETER);
+    
+    /* Set local Name */
+    ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME, (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+            
+    /* Setup advertising parameters:  not connectable */
+    ble.gap().setAdvertisingType(GapAdvertisingParams::ADV_NON_CONNECTABLE_UNDIRECTED);
+    ble.gap().setAdvertisingInterval(ADVERTISE_INTERVAL_IN_MS); 
+}
+
+//On BLE Init Completed
+void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
+{
+    BLE &ble          = params->ble;
+    ble_error_t error = params->error;
+
+    if (error != BLE_ERROR_NONE) {
+        return;
+    }
+
+    //Re advertising again when client is disconnecting
+    //Useful when client is connecting to this Beacon, in this case...not useful nobody's connecting...
+    //ble.gap().onDisconnection(disconnectionCallback);
+        
+    generateAdvertisingData(ble);   
+    
+    /* Ok Advertise now !! */
+    ble.gap().startAdvertising();     
+}
+
+void pollDataCallBack(void)
+{
+    /* Note that the pollDataCallBack() executes in interrupt context, so it is safer to do
+     * heavy-weight sensor polling from the main thread. */
+    triggerSensorPolling = true;
+}
+
+void manageAdvertisingCallBack(void)
+{
+    /* Note that the manageAvertisingCallBack() executes in interrupt context, so it is safer to do
+     * heavy-weight sensor polling from the main thread. */
+    triggerManageAvertising = true;
+}
+
+/* OLD VERSION
+uint16_t read_bat_volt(void)
+{
+    //10 bit resolution, route Vdd as analog input, set ADC ref to VBG band gap
+    //disable analog pin select "PSEL" because we're using Vdd as analog input
+    //no external voltage reference                  
+    NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Enabled;
+    NRF_ADC->CONFIG = (ADC_CONFIG_RES_10bit << ADC_CONFIG_RES_Pos) |
+                      (ADC_CONFIG_INPSEL_SupplyOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos) |
+                      (ADC_CONFIG_REFSEL_VBG << ADC_CONFIG_REFSEL_Pos) |
+                      (ADC_CONFIG_PSEL_Disabled << ADC_CONFIG_PSEL_Pos) |
+                      (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos);
+
+    //NRF_ADC->CONFIG     &= ~ADC_CONFIG_PSEL_Msk;
+    //NRF_ADC->CONFIG     |= ADC_CONFIG_PSEL_Disabled << ADC_CONFIG_PSEL_Pos;
+    NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Enabled;
+    NRF_ADC->TASKS_START = 1;
+    
+    
+    //while loop doesn't actually loop until reading comlete, use a wait.
+    while (((NRF_ADC->BUSY & ADC_BUSY_BUSY_Msk) >> ADC_BUSY_BUSY_Pos) == ADC_BUSY_BUSY_Busy) {};
+    wait_ms(1);
+
+    //save off RESULT before disabling.
+    //uint16_t myresult = (uint16_t)NRF_ADC->RESULT;
+    
+    //disable ADC to lower bat consumption
+    NRF_ADC->TASKS_STOP = 1;
+    NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Disabled;    //disable to shutdown ADC & lower bat consumption
+    
+    return (uint16_t)NRF_ADC->RESULT; // 10 bit
+    //return myresult;
+}  //end read_bat_volt
+*/
+
+// 20191130 - Fonction de lecture du voltage controlé avec le générateur de labo
+// Lecture OK de 1.8 à 3.3 v
+float read_bat_volt(void){
+    NRF_ADC->POWER       = 1;
+    wait_ms(5);
+    NRF_ADC->EVENTS_END  = 0;
+    NRF_ADC->TASKS_STOP  = 1;
+    NRF_ADC->ENABLE;     // save register value
+    NRF_ADC->CONFIG;     // save register value
+    NRF_ADC->RESULT;     // save register value
+    NRF_ADC->ENABLE = ADC_ENABLE_ENABLE_Enabled;
+    NRF_ADC->CONFIG = (ADC_CONFIG_RES_10bit << ADC_CONFIG_RES_Pos) |
+                      (ADC_CONFIG_INPSEL_SupplyOneThirdPrescaling << ADC_CONFIG_INPSEL_Pos) |
+                      (ADC_CONFIG_REFSEL_VBG << ADC_CONFIG_REFSEL_Pos) |
+                      (ADC_CONFIG_PSEL_Disabled << ADC_CONFIG_PSEL_Pos) |
+                      (ADC_CONFIG_EXTREFSEL_None << ADC_CONFIG_EXTREFSEL_Pos);
+    NRF_ADC->EVENTS_END  = 0;
+    NRF_ADC->TASKS_START = 1;
+    while (!NRF_ADC->EVENTS_END) {;}
+    uint16_t wrk = NRF_ADC->RESULT;          // 10 bit result
+    NRF_ADC->EVENTS_END  = 0;
+    NRF_ADC->TASKS_STOP = 1;
+    NRF_ADC->CONFIG      = 0x18;
+    NRF_ADC->EVENTS_END  = 1;
+    NRF_ADC->TASKS_STOP  = 1;
+    NRF_ADC->ENABLE      = 0;
+    wait_ms(5);
+    NRF_ADC->POWER       = 0;
+    wait_ms(5);
+   
+    return ((float)wrk / 1024 * 1.2 * 3);            
+}
+
+//read and convert battery voltage
+float get_battery_level() {
+            
+    float bat_reading = (float)read_bat_volt();    
+    //bat_reading = (bat_reading * 3.6) / 1024.0;
+    
+    #if DEBUG_MODE
+       _device.printf("battery reading: %f \r\n", bat_reading);
+    #endif    
+    
+    return bat_reading;
+    
+    //write battery voltage
+    //uint8_t total_chars;
+    //memset(&bat_volt_char[0], 0, sizeof(bat_volt_char));      //clear out buffer
+    //convert battery voltage float value to string reprsentation to 2 decimal places, and save the size of string.
+    //total_chars = sprintf (bat_volt_char, "%.2f", bat_reading);
+}
+
+int main() {
+   
+   #if DEBUG_MODE
+      _device.baud(9600);
+      _device.printf("**************************");
+      _device.printf("Initialising BLE Sensor...");
+      _device.printf("\r\n");
+   #endif
+   
+   //Init new BLE Instance 
+   BLE& ble = BLE::Instance(BLE::DEFAULT_INSTANCE);
+   ble.init(bleInitComplete);
+   
+   /* SpinWait for initialization to complete. This is necessary because the
+   * BLE object is used in the main loop below. */
+   while (ble.hasInitialized() == false) { /* spin loop */ }
+   
+   //every X seconds, sends period update, up to 1800 (30 minutes)
+   //_pollDataTimer.attach(pollDataCallBack, POLL_DATA_INTERVAL_IN_S);  //send updated I/O every x seconds
+   
+   //Poll some data at beginning
+   triggerSensorPolling = true;
+   
+   #if DEBUG_MODE
+      _device.printf("BLE Sensor initialized !");
+      _device.printf("\r\n");
+   #endif
+   
+   ble.gap().getAddress(0,mac_reverse);  //last byte of MAC (as shown on phone app) is at mac[0], not mac[6];
+   #if DEBUG_MODE
+      _device.printf("MAC Address = ");
+      for (int i=0; i<6; i++) { //prints out MAC address in reverse order; opps.
+         _device.printf("%x:", mac_reverse[i]);
+      }
+      _device.printf("\r\n");
+    #endif
+   
+   // Main infinite loop
+   while (true) {
+       
+       //Time to poll data from sensor
+       if (triggerSensorPolling) {
+           
+           //Is it useful??
+           _pollDataTimer.attach(pollDataCallBack, POLL_DATA_INTERVAL_IN_S);  //send updated I/O every x seconds
+           
+           //Get Battery Level
+           _battery = get_battery_level();
+           
+           #if DEBUG_MODE
+              _device.printf("Battery voltage : %6.2f \r\n", _battery);
+           #endif
+           
+           _temperature = _sensor.sample_ctemp();
+           
+           #if DEBUG_MODE
+              _device.printf("Temperature : %6.2f \r\n", _temperature);
+           #endif
+           
+           _humidity = _sensor.sample_humid();
+           
+           #if DEBUG_MODE
+              _device.printf("Humidity percentage : %d \r\n", _humidity);
+           #endif
+           
+           //Generate new advertising data
+           generateAdvertisingData(ble);
+           
+           /* Ok Advertise now !! */
+           ble.gap().startAdvertising();                                 
+                                 
+           triggerSensorPolling = false;
+           
+           /* trigger turn off advertisement after X seconds */
+           _manageAdvertising.attach(manageAdvertisingCallBack, STOP_ADV_INTERVAL_IN_S); 
+       }
+       
+       if (triggerManageAvertising) {
+           //Stop advertising
+           ble.gap().stopAdvertising();
+           _manageAdvertising.detach();
+           triggerManageAvertising = false;
+       }
+       
+       ble.waitForEvent(); // low power wait for event
+   }
+}
--- sketches/mbed_ble_hub/main.cpp
+++ sketches/mbed_ble_hub/main.cpp
@@ -7,6 +7,8 @@
 * Creation             : 20181215
 *
 * Modification         : 20180120 - Change battery from percent to voltage
+*                        20190310 - Add Reedswitch sensors
+*                        20201018 - Change Office mac address
 *
 */
 
@@ -15,22 +17,32 @@
 #include "mbed.h"
 #include "ble/BLE.h"
 
+//Reedswitch sensor
+//c5:f7:7b:9b:24:46 - Salon main door
+BLEProtocol::AddressBytes_t  reedswitch_sensors_address [] = {
+    //Upstairs
+    //Downstairs
+    {0x46, 0x24, 0x9b, 0x7b, 0xf7, 0xc5} //Salon main door   
+};
+
+//Environmental sensor
 //d2:48:c8:a5:35:4c - M
 //c7:b9:43:94:24:3a - N
 //e9:3d:63:97:39:5e - P
-BLEProtocol::AddressBytes_t  home_sensors_address [] = { //Careful !! Little Indian Order
+BLEProtocol::AddressBytes_t  env_sensors_address [] = { //Careful !! Little Indian Order
                                                            
                                                            //Upstairs 
-                                                           {0x4c, 0x35, 0xa5, 0xc8, 0x48, 0xd2},
-                                                           {0x3a, 0x24, 0x94, 0x43, 0xb9, 0xc7},
-                                                           {0x5e, 0x39, 0x97, 0x63, 0x3d, 0xe9},
-                                                           {0x4d, 0x2c, 0xff, 0xdc, 0x15, 0xd8} //Bathroom                                                           
+                                                           //{0x4c, 0x35, 0xa5, 0xc8, 0x48, 0xd2},
+                                                           //{0x3a, 0x24, 0x94, 0x43, 0xb9, 0xc7},
+                                                           //{0x5e, 0x39, 0x97, 0x63, 0x3d, 0xe9},
+                                                           //{0x4d, 0x2c, 0xff, 0xdc, 0x15, 0xd8} //Bathroom                                                           
                                                            
                                                            //Downstairs
-                                                           //{0x6a, 0xd8, 0x6f, 0xc6, 0xa4, 0xf4}, //Salon
-                                                           //{0x8b, 0xef, 0x1d, 0x6d, 0xa6, 0xd4} //Bureau
+                                                           {0x6a, 0xd8, 0x6f, 0xc6, 0xa4, 0xf4}, //Salon
+                                                           {0xc1, 0x99, 0x0e, 0xcf, 0xe0, 0xd7} //Bureau
                                                            //{0x46, 0x24, 0x9b, 0x7b, 0xf7, 0xc5} //Testing purpose
                                                         };
+
 
 Serial device(p0, p1);  //nRF51822 uart:  TX=p0.  RX=p1
 //DigitalIn pinHandShake(p9);  //handshake uart to prevent output before bridge MCU is ready.  Flow control.
@@ -48,17 +60,150 @@ void advertisementCallback(const Gap::AdvertisementCallbackParams_t *params) {
     // byte 1: The key, it is the type of the data
     // byte [2..N] The value. N is equal to byte0 - 1
     
-    bool isSensorMine = false;
-    size_t size = sizeof(home_sensors_address) / sizeof(BLEProtocol::AddressBytes_t);
+    bool isEnvSensorMine = false;
+    size_t size = sizeof(env_sensors_address) / sizeof(BLEProtocol::AddressBytes_t);
     for (int i=0;i<size;i++) {
         
-        if (0 == memcmp(params->peerAddr, home_sensors_address[i], sizeof (home_sensors_address[i]))) {           
-           isSensorMine = true;
+        if (0 == memcmp(params->peerAddr, env_sensors_address[i], sizeof (env_sensors_address[i]))) {           
+           isEnvSensorMine = true;
            break;
         }
-    }    
+    }
     
-    if (isSensorMine) {
+    bool isReedswitchSensorMine = false;
+    size = sizeof(reedswitch_sensors_address) / sizeof(BLEProtocol::AddressBytes_t);
+    for (int i=0;i<size;i++) {
+        
+        if (0 == memcmp(params->peerAddr, reedswitch_sensors_address[i], sizeof (reedswitch_sensors_address[i]))) {           
+           isReedswitchSensorMine = true;
+           break;
+        }
+    }
+    
+    if (isReedswitchSensorMine) {
+        
+        //Publish data via uart
+        device.printf("{");
+        
+        device.printf("\"Address\":\"%02x:%02x:%02x:%02x:%02x:%02x\"",
+                    params->peerAddr[5], 
+                    params->peerAddr[4], 
+                    params->peerAddr[3], 
+                    params->peerAddr[2], 
+                    params->peerAddr[1], 
+                    params->peerAddr[0]);
+        
+        device.printf(",");
+        //device.printf("\r\n");
+        device.printf("\"Rssi\":");
+        device.printf("\"%d\"",params->rssi);
+        
+        uint8_t serviceDataOrder = 1;
+        //Parse advertising payload...we are looking for service data
+        for (uint8_t i = 0; i < params->advertisingDataLen; ++i) {
+
+            const uint8_t record_length = params->advertisingData[i];
+            if (record_length == 0) {
+                continue;
+            }
+            
+            const uint8_t type = params->advertisingData[i + 1];
+            const uint8_t* value = params->advertisingData + i + 2;
+            const uint8_t value_length = record_length - 1;
+            
+            if (type == GapAdvertisingData::SERVICE_DATA) {
+                char dest[10];
+                char buffer_tmp[10];
+                char buffer[10];
+                memcpy(dest, value, value_length);
+                dest[value_length] = '\0'; //null terminate string
+                
+                size_t len = strlen(dest);
+                
+                if (serviceDataOrder == 1) {
+                    device.printf(",");
+                    //device.printf("\r\n");
+                    device.printf("\"Battery\":");
+                }
+                
+                if (serviceDataOrder == 2) {
+                    device.printf(",");
+                    //device.printf("\r\n");
+                    device.printf("\"State\":");
+                }
+                
+                device.printf("\"");
+                //j begin at 2 because we skip service data uuid
+                uint8_t cpt = 0;
+                
+                for (uint8_t j = 2; j < len; ++j) {
+                    
+                    #if DEBUG_MODE                    
+                        device.printf("%02x", dest[len - j + 1]); //reverse little indian order 
+                    #endif
+                                                       
+                    sprintf(buffer_tmp, "%02x", dest[len - j + 1]); //convert to hex
+                    
+                    //Assemble hex string
+                    for(uint8_t z =0; z < strlen(buffer_tmp);++z) {
+                        buffer[cpt] = buffer_tmp[z];
+                        cpt++;
+                    }
+                }      
+                buffer[cpt] = '\0';  //null terminate string
+                
+                //Convert to float
+                float valueFloat = (float)strtol(buffer, 0, 16);
+                
+                //Convert back to string
+                char valueString[10];
+                
+                //Battery
+                if (serviceDataOrder == 1) {
+                    float valueFloat = valueFloat / 100;
+                    sprintf(valueString, "%.2f", valueFloat);
+                }
+                
+                //State
+                if (serviceDataOrder == 2) {
+                    sprintf(valueString, "%.0f", valueFloat);
+                }
+                
+                device.printf("%s", valueString); 
+                
+                #if DEBUG_MODE             
+                    device.printf("\r\n Buffer : %s \r\n", buffer);   
+                    device.printf("\r\n Value String : %s \r\n", valueString); 
+                #endif 
+                
+                device.printf("\"");
+                
+                serviceDataOrder++;
+            }
+        
+            //Next record!
+            i += record_length;
+        }
+        
+        device.printf("}");
+        device.printf("\r\n"); //End of line
+        
+        //pin led ON
+        //pinLed = 0; 
+        
+        wait_ms(60);  //needed to give gateway time to assert flow control handshake pin
+        //while (pinHandShake.read() == 1)    //normally pulled down, so loop when gateway processing;
+        //{
+            //uart flow control
+            //blocking until gateway has processed ADV data from uart
+        //}
+        
+        //pin led OFF
+        //pinLed = 1;
+        
+    }
+    
+    if (isEnvSensorMine) {
         
         //Publish data via uart
         device.printf("{");
