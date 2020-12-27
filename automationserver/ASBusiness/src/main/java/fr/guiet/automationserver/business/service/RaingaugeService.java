2bf67277265e1942d90714a1e7f774b3fd70cc55
fguiet@gmail.com <fguiet@gmail.com>
2020-12-27 15:10:40 +0100

    change duplicate message detection

--- automationserver/ASBusiness/src/main/java/fr/guiet/automationserver/business/service/RaingaugeService.java
+++ automationserver/ASBusiness/src/main/java/fr/guiet/automationserver/business/service/RaingaugeService.java
@@ -181,7 +181,7 @@ public class RaingaugeService implements Runnable, IMqttable {
 						messageProcessed = true;
 						return messageProcessed;
 					}*/
-					if (DateUtils.secondsBetweenDate(_lastMessageReceived, new Date()).compareTo(new Long(4)) < 0) {
+					if (DateUtils.secondsBetweenDate(_lastMessageReceived, new Date()).compareTo(new Long(2)) < 0) {
 						_logger.info("Less than two second ago I received a message from Raingauge sensor...skipping this one...");
 						messageProcessed = true;
 						return messageProcessed;
